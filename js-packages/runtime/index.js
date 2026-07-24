f747fc388a97cbfd67c3aea0aa89da6a68867c4a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-24 21:59:26 +0200

    Create index.js

---
+++ js-packages/runtime/index.js
@@ -0,0 +1,9 @@
+// js-packages/runtime/index.js
+
+//
+export { default as Enum   } from './types/Enum.js';
+export { default as List   } from './types/List.js';
+export { default as Record } from './types/Record.js';
+export { default as Struct } from './types/Struct.js';
+export { default as Tuple  } from './types/Tuple.js';
+export { default as Union  } from './types/Union.js';
