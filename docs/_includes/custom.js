3d8c7fb72824ba755029f4ef6e97f70081ac87ec
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-24 16:36:10 +0200

    Update custom.js

--- docs/_includes/custom.js
+++ docs/_includes/custom.js
@@ -21,6 +21,77 @@ poo.operators = `
 `;
 
 hljs.registerLanguage('poo', function (hljs) {
+
+  const VARIABLE_INTERPOLATION = {
+    className : 'variable',
+    begin     : /\$[a-zA-Z_][a-zA-Z0-9_]*/
+  };
+
+  // 2. Vorab-Deklaration für die komplexe Interpolation ${ ... }
+  const EXPRESSION_INTERPOLATION = {
+    className: 'subst',
+    begin: /\\$\{/,
+    end: /\}/,
+    keywords: {
+      keyword:  "as and break catch continue cpy do fail fn if kill loop in new obj of on or pkg ref return skip static switch use val yield",
+      literal:  "false null true undefined",
+      built_in: "Array Blob Bool Char Color Date Enum Generator List Map Number Queue Pattern Record RegExp Set Stack String Store Symbol Tree Tuple Union"
+    },
+    contains: [] // Wird gleich unten dynamisch befüllt, um Rekursion zu erlauben
+  };
+
+  const STRING_DOUBLE = {
+    className: 'string',
+    begin: '"', end: '"',
+    contains: [hljs.BACKSLASH_ESCAPE, VARIABLE_INTERPOLATION, EXPRESSION_INTERPOLATION]
+  };
+
+  const STRING_SINGLE = {
+    className: 'string',
+    begin: "'", end: "'",
+    contains: [hljs.BACKSLASH_ESCAPE, VARIABLE_INTERPOLATION, EXPRESSION_INTERPOLATION]
+  };
+
+  const STRING_BACKTICK = {
+    className: 'string',
+    begin: '`', end: '`',
+    contains: [hljs.BACKSLASH_ESCAPE, VARIABLE_INTERPOLATION, EXPRESSION_INTERPOLATION]
+  };
+
+  EXPRESSION_INTERPOLATION.contains = [
+    hljs.COMMENT('//', '$'),
+    STRING_DOUBLE,
+    STRING_SINGLE,
+    STRING_BACKTICK,
+    { className: 'keyword', begin: /fn[*^]/ },
+    { className: 'number', begin: '0[xX][0-9a-fA-F_]+|0[bB][01_]+|\\d[\\d_]*\\.\\d[\\d_]*(?:[eE][+-]?\\d+)?|\\d[\\d_]*' },
+    { className: 'operator', begin: buildOperatorRegex(poo.operators) },
+    { className: 'punctuation', begin: /[{}[\]();:,.]/ }
+  ];
+
+  return {
+    name: "Poo",
+    case_insensitive: false,
+    keywords: {
+      keyword:  "as and break catch continue cpy do fail fn if kill loop in new obj of on or pkg ref return skip static switch use val yield",
+      literal:  "false null true undefined",
+      built_in: "Array Blob Bool Char Color Date Enum Generator List Map Number Queue Pattern Record RegExp Set Stack String Store Symbol Tree Tuple Union"
+    },
+    contains: [
+      hljs.COMMENT('//', '$'),
+      { className: 'keyword', begin: /fn[*^]/ },
+      STRING_DOUBLE,
+      STRING_SINGLE,
+      STRING_BACKTICK,
+      { className: 'number', begin: '0[xX][0-9a-fA-F_]+|0[bB][01_]+|\\d[\\d_]*\\.\\d[\\d_]*(?:[eE][+-]?\\d+)?|\\d[\\d_]*' },
+      { className: 'operator', begin: buildOperatorRegex(poo.operators) },
+      { className: 'punctuation', begin: /[{}[\]();:,.]/ }
+    ]
+  };
+  
+});
+
+hljs.registerLanguage('poo', function (hljs) {
   return {
     name: "Poo",
     case_insensitive: false,
