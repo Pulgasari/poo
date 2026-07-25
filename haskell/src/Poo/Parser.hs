5225c2ac6b6be049747f6d72083402284953caf3
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-25 17:24:58 +0200

    Update Parser.hs

--- haskell/src/Poo/Parser.hs
+++ haskell/src/Poo/Parser.hs
@@ -33,7 +33,7 @@ pLiteral = token (\case
   _           -> Nothing) mempty
 
 -- ============================================================
--- Expressions with Operator Precedence
+-- Expressions with Precedence + Unary + Collections
 -- ============================================================
 
 -- Precedence levels (from lowest to highest):
@@ -48,7 +48,7 @@ pLiteral = token (\case
 pExpr :: Parser Expr
 pExpr = pPipe
 
--- Level 1: Pipe (left-associative)
+-- Level 1: Pipe
 pPipe :: Parser Expr
 pPipe = do
   left <- pOr
@@ -83,23 +83,22 @@ pAdd = pBinaryOp pMul
 
 -- Level 6: Multiplication / Division / Modulo
 pMul :: Parser Expr
-pMul = pBinaryOp pApp
+pMul = pBinaryOp pUnary
   [ (TStar,    Mul)
   , (TSlash,   Div)
   , (TPercent, Mod)
   ]
 
--- Helper for left-associative binary operators
-pBinaryOp :: Parser Expr -> [(Token, BinOp)] -> Parser Expr
-pBinaryOp higher ops = do
-  left <- higher
-  rest <- many $ do
-    op  <- choice [tok t *> pure binOp | (t, binOp) <- ops]
-    right <- higher
-    pure (op, right)
-  pure $ foldl (\acc (op, right) -> Binary acc op right) left rest
+-- Level 7: Unary operators
+pUnary :: Parser Expr
+pUnary = choice
+  [ do tok TMinus
+       e <- pUnary
+       pure (Unary Neg e)
+  , pApp
+  ]
 
--- Level 7: Function application + Atoms
+-- Level 8: Function application
 pApp :: Parser Expr
 pApp = do
   func <- pAtom
@@ -108,15 +107,98 @@ pApp = do
     [] -> func
     _  -> App func args
 
+-- Atoms (literals, variables, parentheses, collections, if)
 pAtom :: Parser Expr
 pAtom = choice
   [ Lit <$> pLiteral
   , Var <$> pIdent
-  , do tok TLParen
-       e <- pExpr
-       tok TRParen
-       pure e
+  , pParens
+  , pArray
+  , pList
+  , pTuple
+  , pRecord
+  , pIf
   ]
+
+pParens :: Parser Expr
+pParens = do
+  tok TLParen
+  e <- pExpr
+  tok TRParen
+  pure e
+
+-- -------------------- Collections --------------------
+
+pArray :: Parser Expr
+pArray = do
+  tok TLBracket
+  elems <- pExpr `sepBy` tok TComma
+  tok TRBracket
+  pure (Array elems)
+
+pList :: Parser Expr
+pList = do
+  tok THashLBracket
+  elems <- pExpr `sepBy` tok TComma
+  tok TRBracket
+  pure (List elems)
+
+pTuple :: Parser Expr
+pTuple = do
+  tok THashLParen
+  elems <- pExpr `sepBy` tok TComma
+  tok TRParen
+  pure (Tuple elems)
+
+pRecord :: Parser Expr
+pRecord = do
+  tok THashLBrace
+  fields <- pField `sepBy` tok TComma
+  tok TRBrace
+  pure (Record fields)
+
+pField :: Parser (Name, Expr)
+pField = do
+  name <- pIdent
+  tok TColon
+  value <- pExpr
+  pure (name, value)
+
+-- -------------------- if / or --------------------
+
+pIf :: Parser Expr
+pIf = do
+  tok TIf
+  cond <- pExpr
+  thenBranch <- pThenBranch
+  elseBranch <- optional pOrBranch
+  pure (If cond thenBranch elseBranch)
+
+pThenBranch :: Parser Expr
+pThenBranch = choice
+  [ do tok TDo
+       pExpr
+  , pBlock
+  ]
+
+pOrBranch :: Parser Expr
+pOrBranch = do
+  tok TOr
+  -- either "or <cond> ..." or just "or ..."
+  choice
+    [ try $ do
+        cond <- pExpr
+        branch <- pThenBranch
+        pure (If cond branch Nothing)   -- simplified for now
+    , pThenBranch
+    ]
+
+pBlock :: Parser Expr
+pBlock = do
+  tok TLBrace
+  stmts <- many pStmt
+  tok TRBrace
+  pure (Block stmts)
 
 -- Statements
 pVal :: Parser Stmt
@@ -150,7 +232,11 @@ pParams = choice
   ]
 
 pStmt :: Parser Stmt
-pStmt = choice [pVal, pFn, ExprStmt <$> pExpr <* tok TSemicolon]
+pStmt = choice
+  [ pVal
+  , pFn
+  , ExprStmt <$> pExpr <* optional (tok TSemicolon)
+  ]
 
 pProgram :: Parser Program
 pProgram = many pStmt <* eof
