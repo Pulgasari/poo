5e539b7950591b9f39854edbcc7f3cef1bb3f5ff
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-25 16:51:24 +0200

    Create Token.hs

---
+++ haskell/src/Token.hs
@@ -0,0 +1,39 @@
+module Poo.Token where
+
+data Token
+  -- Literals & Identifiers
+  = TInt Integer
+  | TFloat Double
+  | TString String
+  | TBool Bool
+  | TNull
+  | TUndefined
+  | TIdent String
+
+  -- Keywords
+  | TVal
+  | TFn
+  | TIf
+  | TOr
+  | TLoop
+  | TDo
+  | TAs
+  | TReturn
+
+  -- Operators
+  | TPlus | TMinus | TStar | TSlash | TPercent
+  | TEq | TNeq | TLt | TGt | TLe | TGe
+  | TAnd | TOrOp
+  | TPipe                    -- >>
+  | TAssign                  -- =
+
+  -- Delimiters
+  | TLParen | TRParen
+  | TLBrace | TRBrace
+  | TLBracket | TRBracket
+  | TComma | TSemicolon
+  | TArrow                   -- =>
+  | THash                    -- #
+
+  | TEOF
+  deriving (Show, Eq)
