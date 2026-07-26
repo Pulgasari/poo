a9f8817f2e7b55c3568085a6a6f6a2fd8ed84d4a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 17:29:18 +0200

    Update index.js

--- js-packages/worker/index.js
+++ js-packages/worker/index.js
@@ -1,1 +1,20 @@
-// js-packages/worker/index.js
+// @poo/worker
+
+// Service Worker entry point. Registers standard install/activate
+// handlers (immediate takeover, no waiting on old clients to close) and
+// intercepts fetches for ".poo" files - see intercept.js.
+
+import { shouldIntercept, handleFetch } from './intercept.js';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (!shouldIntercept(event.request)) return;
+  event.respondWith(handleFetch(event));
+});
