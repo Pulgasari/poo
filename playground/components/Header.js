aa014fb6524b74f8b6bcfe371a7b28f51f3b4d75
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-27 17:55:29 +0200

    Update Header.js

--- playground/components/Header.js
+++ playground/components/Header.js
@@ -1,5 +1,13 @@
 // components/Header.js
 
 export default function () {
-  return html ``;
+  return html `
+    <div id='app-header'>
+      POO
+      <nav>
+        <${Link} href='docs'       label='Docs'       />
+        <${Link} href='playground' label='Playground' />
+      </nav>
+    </div>
+  `;
 }
