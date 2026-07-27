851ac40a5858e603833cff8e234911ac3c41c611
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-27 18:48:26 +0200

    Update Link.js

--- playground/components/Link.js
+++ playground/components/Link.js
@@ -1,5 +1,9 @@
 // components/Link.js
 
+const base = 'https://pulgasari.github.io/poo/';
+
 export default function ({ href, label, children }) {
+  href = href.startsWith('https://') ? href : (base + href);
+  
   return html `<a href=${href}>${children || label}</a>`;
 }
